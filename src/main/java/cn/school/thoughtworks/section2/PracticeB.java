4f9983c4c8b47fe721e09aee4594618d44fabb4c
lml <1227818164@qq.com>
2018-03-30 18:30:36 +0800

    completed section2

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,44 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : collection1) {
+            int index = s.indexOf("-");
+            if (index == -1 ) {
+                if (map.containsKey(s)) {
+                    map.put(s, map.get(s) + 1);
+                } else {
+                    map.put(s, 1);
+                }
+            } else {
+                String key = s.substring(0, index);
+                String value = s.substring(index + 1);
+                if (isNum(value)) {
+                    map.put(key, Integer.valueOf(value));
+                }else {
+                    if (map.containsKey(s)) {
+                        map.put(s, map.get(s) + 1);
+                    } else {
+                        map.put(s, 1);
+                    }
+                }
+            }
+        }
+        return map;
+    }
 
-        return null;
+    public static boolean isNum(String str) {
+        try {
+            Integer.valueOf(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,65 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
+    public static Map<String, Integer> map = new HashMap<>();
+
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        int index;
+        for (String s : collection1) {
+            if ((index = s.indexOf("-")) > 0) {
+                String key = s.substring(0, index);
+                String value = s.substring(index + 1);
+                if (isNum(value)) {
+                    putMap(key, Integer.valueOf(value));
+                } else {
+                    putMap(s,1);
+                }
+            }else if ((index = s.indexOf(":")) > 0) {
+                String key = s.substring(0, index);
+                String value = s.substring(index + 1);
+                if (isNum(value)) {
+                    putMap(key, Integer.valueOf(value));
+                } else {
+                    putMap(s,1);
+                }
+            }else if ((index = s.indexOf("[")) > 0) {
+                String key = s.substring(0, index);
+                String value = s.substring(index + 1, s.length() - 1);
+                if (isNum(value)) {
+                    putMap(key, Integer.valueOf(value));
+                } else {
+                    putMap(s,1);
+                }
+            }else {
+                putMap(s, 1);
+            }
+        }
+        return map;
+    }
+    /*
+    判断字符串是否能转化数字
+    */
+    public static boolean isNum(String str) {
+        try {
+            Integer.valueOf(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+   /*
+   更新map
+   */
+    public static void putMap(String str,int nums){
+        if (map.containsKey(str)) {
+            map.put(str, map.get(str) + nums);
+        } else {
+            map.put(str, nums);
+        }
     }
 }
